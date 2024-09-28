38bb16f84a08e6cbc0f5dea44d7dee11eb650c0a
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-09-28 04:08:05 +0300

    Update DbConnection.js

--- config/DbConnection.js
+++ config/DbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const DBConnection = ()=>{
-    mongoose.connect("mongodb://localhost:27017/LNK").then((connect)=>{
+    mongoose.connect("mongodb://127.0.0.1:27017/LNK").then((connect)=>{
         console.log(`DataBase Connected ${connect.connection.host}`)
     })
 }
