7cd001fae7d01798400aaa2d141b7ac6ef6b7bbe
Mahmoud Ahmed <73077353+mahmoudahme@users.noreply.github.com>
2024-09-28 04:01:43 +0300

    Update DbConnection.js

--- config/DbConnection.js
+++ config/DbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const DBConnection = ()=>{
-    mongoose.connect("mongodb://MahmoudAhmed:mahm1234@cluster0-shard-00-00.qgzok.mongodb.net:27017,cluster0-shard-00-01.qgzok.mongodb.net:27017,cluster0-shard-00-02.qgzok.mongodb.net:27017/Tawaad?ssl=true&replicaSet=atlas-c9i7c6-shard-0&authSource=admin&retryWrites=true&w=majority").then((connect)=>{
+    mongoose.connect("mongodb://localhost:27017/LNK").then((connect)=>{
         console.log(`DataBase Connected ${connect.connection.host}`)
     })
 }
